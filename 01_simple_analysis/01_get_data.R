560c44816ebc115078695aae603bb834f81e4bcf
aliciammar <aliciamarley@gmail.com>
2025-07-07 09:47:20 -0700

    download satellite image

--- 01_simple_analysis/01_get_data.R
+++ 01_simple_analysis/01_get_data.R
@@ -37,7 +37,7 @@ mapview(spanish_muni_sf)
 
 ## Get Tenerife Island
 tenerife_sf <- gisco_get_nuts(
-    country = "Spain",
+    country    = "Spain",
     resolution = "01",
     nuts_level = 3
 ) |> 
@@ -76,10 +76,28 @@ mapview(selected_muni_sf)
 ## 3.2. Get satellite image -------------------
 
 ## Download Sentinel-2 image
+tic() # Outputs the computation time from tic() to toc()
+sentinel_path <- get_sentinel2_imagery(
+    aoi             = selected_muni_sf, # area of interest (AOI)
+    start_date      = "2024-05-04", # time window to pull satellite photo from
+    end_date        = "2024-05-05",
+    output_filename = str_glue("data/sentinel/{selected_muni_sf$id}.tif")
+)
+toc()
 
+## Read
+sentinel_sr <- rast(sentinel_path)
 
 ## Visualize
+(sentinel_sr / 10000) |> # Apparently sentinel data are stored multiplied by 10,000 so there are no                         floats (numbers with decimals)--maybe it uses less storage/is a smaller file                         this way?
+    plotRGB(4, 3, 2, stretch = "lin") 
 
+plot(
+    st_geometry(selected_muni_sf),
+    add    = TRUE,
+    border = "red",
+    lwd    = 2
+    )
 
 # 4. Export ---------------------------------------------------------------
 
