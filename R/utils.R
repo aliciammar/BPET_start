a30f239a2e2b5b350c68ae34227c7401d0ba8b72
aliciammar <aliciamarley@gmail.com>
2025-07-09 18:57:42 -0700

    calculate ndvi

--- R/utils.R
+++ R/utils.R
@@ -45,3 +45,18 @@ get_sentinel2_muni <- function(data) {
     rast(sentinel_path) / 10000
     
 }
+
+## FUNCTION: Calculate NDVI
+calculate_ndvi <- function(data) {
+    
+    ## Calculate NDVI
+    ## Formula: NDVI = (N - R) / (N + R)
+    ndvi_sr <- (data$N - data$R) / (data$N + data$R)
+     
+    ## Rename band
+    names(ndvi_sr) <- "NDVI"
+    
+    ## Return NDVI
+    ndvi_sr
+    
+}
